b0d8d75f652101f92a1226225e42e4c8dc0ac84b
Harshit Gupta <hgupta07@in.ibm.com>
2020-09-08 23:50:35 +0530

    Encode example

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -37,6 +39,15 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, r *http.Request) {
+
+	p1 := person{
+		First: "Jenny",
+	}
+
+	err := json.NewEncoder(w).Encode(p1)
+	if err != nil {
+		log.Println("Encode Bad Data")
+	}
 
 }
 
