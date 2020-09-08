4a49e22e3c9cbe2bde3cd6b6c101e4d6391adf30
Harshit Gupta <hgupta07@in.ibm.com>
2020-09-08 23:41:22 +0530

    Set up server

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"net/http"
 )
 
 type person struct {
@@ -11,25 +9,37 @@ type person struct {
 }
 
 func main() {
-	p1 := person{
-		First: "Jenny",
-	}
+	// p1 := person{
+	// 	First: "Jenny",
+	// }
 
-	p2 := person{
-		First: "James",
-	}
+	// p2 := person{
+	// 	First: "James",
+	// }
 
-	xp := []person{p1, p2}
+	// xp := []person{p1, p2}
 
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Println(string(bs))
+	// fmt.Println(string(bs))
 
-	xp2 := []person{}
-	json.Unmarshal(bs, &xp2)
+	// xp2 := []person{}
+	// json.Unmarshal(bs, &xp2)
 
-	fmt.Println("Back to go", xp2)
+	// fmt.Println("Back to go", xp2)
+
+	http.HandleFunc("/encode", foo)
+	http.HandleFunc("/decode", bar)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
 }
